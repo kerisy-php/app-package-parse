04062a46bb5dcecc6073f3e1bd3f91a3abbcab45
aaron <wubo2@putao.com>
2016-12-16 18:33:17 +0800

    Mod : 修改demo显示效果

--- demo/js/ipa_parser.js
+++ demo/js/ipa_parser.js
@@ -106,6 +106,7 @@
 
 					
 					ipaInfoPlistReader(blob, function(plistObject){
+						fileList.innerHTML = format(JSON.stringify(plistObject));
 						console.log(plistObject);
 					});
 					
@@ -146,29 +147,6 @@
 			});
 
 		}, true);
-
-
-		function loadXMLDoc(xmlFile){
-			var xmlDoc;
-			try{
-				//Internet Explorer 可以使用其原生方法加载 XML
-				xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
-			}catch(e){
-				try{
-					//Firefox 也有标准方法,但可能造成其他浏览器报错,故省略
-					//使用 XMLHttpRequest 替代,适用于大部分浏览器
-					var xmlHttp=new XMLHttpRequest() ;
-					xmlHttp.open("GET",xmlFile,false) ;
-					xmlHttp.send(null) ;
-					return xmlHttp.responseXML;
-				}catch(e){
-					return null;
-				}
-			}
-			xmlDoc.async=false;
-			xmlDoc.load(xmlFile);
-			return xmlDoc;
-		}
 
 	})();
 
